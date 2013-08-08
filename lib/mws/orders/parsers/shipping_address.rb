ecde437fefdb75cb095f34c8da4eb462d20a586e
hakanensari <hakan.ensari@papercavalier.com>
2013-08-09 01:52:03 +0300

    Implement Shipping Address

--- lib/mws/orders/parsers/shipping_address.rb
+++ lib/mws/orders/parsers/shipping_address.rb
@@ -3,21 +3,27 @@ module MWS
     module Parser
       class ShippingAddress < Model
         def name
+          text_at_xpath('Name')
         end
 
         def address_lines
+          1.upto(3).map { |i| at_xpath("AddressLine#{i}") }.compact.map(&:text)
         end
 
         def city
+          text_at_xpath('City')
         end
 
         def state_or_region
+          text_at_xpath('StateOrRegion')
         end
 
         def postal_code
+          text_at_xpath('PostalCode')
         end
 
         def country
+          text_at_xpath('Country')
         end
       end
     end
---
+++ test/parsers/shipping_address_test.rb
@@ -0,0 +1,35 @@
+require_relative 'test_helper'
+
+class ShippingAddressParserTest < ParserTest
+  def setup
+    node = fixture.xpath('//xmlns:ShippingAddress').first
+    @address = Parser::ShippingAddress.new(node)
+  end
+
+  def test_name
+    assert_kind_of String, @address.name
+  end
+
+  def test_address_lines
+    assert_kind_of Array, @address.address_lines
+    @address.address_lines.each do |line|
+      assert_kind_of String, line
+    end
+  end
+
+  def test_city
+    assert_kind_of String, @address.city
+  end
+
+  def test_state_or_region
+    assert_kind_of String, @address.state_or_region
+  end
+
+  def test_postal_code
+    assert_kind_of String, @address.postal_code
+  end
+
+  def test_country
+    assert_kind_of String, @address.country
+  end
+end
