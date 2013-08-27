25be1d4568c0c73daf066085c3e02041925948d7
hakanensari <hakan.ensari@papercavalier.com>
2013-08-27 22:25:07 +0100

    Run integration tests against multiple accounts

--- test/client_test.rb
+++ test/client_test.rb
@@ -2,27 +2,35 @@ require_relative 'test_helper'
 require 'yaml'
 
 class ClientTest < MiniTest::Test
-  def mws_credentials
+  def mws_accounts
     YAML.load_file(File.expand_path('../fixtures/mws.yml', __FILE__))
   end
 
-  def setup
-    @client = Client.new(*mws_credentials.values)
+  def each_client(&blk)
+    mws_accounts.each do |mws|
+      yield Client.new(*mws.values)
+    end
   end
 
   def test_manages_orders
-    @client.list_orders(created_after: Date.today - 365)
-    order = @client.list_orders_by_next_token.first
-    refute_empty @client.get_order(order.amazon_order_id)
+    each_client do |client|
+      client.list_orders(created_after: Date.today - 365)
+      order = client.list_orders_by_next_token.first
+      refute_empty client.get_order(order.amazon_order_id)
+    end
   end
 
   def test_manages_order_items
-    order = @client.list_orders(created_after: Date.today - 365).first
-    refute_empty @client.list_order_items(order.amazon_order_id)
+    each_client do |client|
+      order = client.list_orders(created_after: Date.today - 365).first
+      refute_empty client.list_order_items(order.amazon_order_id)
+    end
   end
 
   def test_gets_service_status
-    status = @client.get_service_status
-    refute_empty status.to_s
+    each_client do |client|
+      status = client.get_service_status
+      refute_empty status.to_s
+    end
   end
 end
