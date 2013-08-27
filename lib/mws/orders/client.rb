53fe8221ffb4d6118743f79041ab2033098f66d4
hakanensari <hakan.ensari@papercavalier.com>
2013-08-27 18:14:08 +0100

    Remove documentation to Client

--- lib/mws/orders/client.rb
+++ lib/mws/orders/client.rb
@@ -3,9 +3,38 @@ module MWS
     class Client < ::Peddler::Orders
       extend Forwardable
 
+      # Public: Gets one or more orders.
+      #
+      # amazon_order_ids - One or more String Amazon order ids.
+      #
+      # Examples
+      #
+      #   client.get_order('123-1234567-1234567')
+      #
+      # Returns Orders.
       def_delegator :orders, :get, :get_order
+
+      # Public: List orders created or updated during a specified time frame.
+      #
+      # params - The Hash request parameters used to narrow the orders list.
+      #          Refer to the MWS Orders API for available parameters.
+      #
+      # Examples
+      #
+      #   client.list_orders(created_after: 1.week.ago)
+      #
+      # Returns Orders.
       def_delegator :orders, :list, :list_orders
+
+      # Public: Lists the next page of orders using the NextToken parameter.
+      #
+      # Examples
+      #
+      #   client.list_orders_by_next_token
+      #
+      # Returns Orders.
       def_delegator :orders, :list_by_next_token, :list_orders_by_next_token
+
       def_delegator :order_items, :list, :list_order_items
       def_delegator :order_items, :list_by_next_token, :list_order_items_by_next_token
       def_delegator :service_status, :get, :get_service_status
--- lib/mws/orders/requests/orders.rb
+++ lib/mws/orders/requests/orders.rb
@@ -4,15 +4,6 @@ module MWS
       class Orders < Base
         include Tokenable
 
-        # Public: Gets specified orders.
-        #
-        # amazon_order_ids - One or more String Amazon order numbers.
-        #
-        # Examples
-        #
-        #   orders.get('123-1234567-1234567')
-        #
-        # Returns an enumerable list of MWS::Orders::Parser::Orders.
         def get(*amazon_order_ids)
           parameters(:get_order)
             .update(amazon_order_id: amazon_order_ids)
@@ -22,16 +13,6 @@ module MWS
           execute
         end
 
-        # Public: List orders created or updated during a specified time frame.
-        #
-        # options - The Hash request parameters used to narrow the orders list.
-        #           See the MWS Orders API for available parameters.
-        #
-        # Examples
-        #
-        #   orders.list(created_after: 1.week.ago)
-        #
-        # Returns an enumerable list of MWS::Orders::Parser::Orders.
         def list(options)
           options[:marketplace_id] ||= client.home_marketplace_id
 
@@ -44,13 +25,6 @@ module MWS
           execute
         end
 
-        # Public: Lists the next page of orders using the NextToken parameter.
-        #
-        # Examples
-        #
-        #   orders.list_by_next_token
-        #
-        # Returns an enumerable list of MWS::Orders::Parser::Orders.
         def list_by_next_token
           parameters(:list_orders_by_next_token)
             .update(next_token: next_token)
