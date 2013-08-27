e01d8b7b24e40405f94a8edff247a91d4f0f6ec8
hakanensari <hakan.ensari@papercavalier.com>
2013-08-27 17:58:35 +0100

    Refactor fixture helper

--- test/parsers/test_helper.rb
+++ test/parsers/test_helper.rb
@@ -2,7 +2,7 @@ require_relative '../test_helper'
 
 class ParserTest < MiniTest::Test
   def fixture(type)
-    file = File.expand_path("../../fixtures/list_#{type}.xml", __FILE__)
+    file = File.expand_path("../../fixtures/#{type}.xml", __FILE__)
     body = File.read(file)
 
     Nokogiri(body)
