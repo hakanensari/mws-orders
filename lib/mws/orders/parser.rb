c37d71da730f439e51c2e30a4ea960dc0dfd7702
Josh Becker <beckerbi@gmail.com>
2019-02-27 10:23:55 -0500

    Expose status code and body of the response

--- lib/mws/orders/parser.rb
+++ lib/mws/orders/parser.rb
@@ -35,6 +35,14 @@ module MWS
         @response.headers
       end
 
+      def status_code
+        @response.status
+      end
+
+      def body
+        @response.body
+      end
+
       private
 
       def find_result_node
